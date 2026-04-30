5fdf77125652251e9369116796f28b1a4b281437
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2026-04-30 04:35:28 -0400

    modified - build command update

--- frontend/src/build/buildSite.js
+++ frontend/src/build/buildSite.js
@@ -10,8 +10,8 @@ fs.writeFileSync("dist/index.html", generateHome());
 fs.writeFileSync("dist/menu-he.html", generateMenu("he"));
 fs.writeFileSync("dist/menu-en.html", generateMenu("en"));
 
-fs.copyFileSync(
+/* fs.copyFileSync(
     "public/main.css",
     "dist/main.css"
-);
+); */
 console.log("Site built successfully 🚀");
